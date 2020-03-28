aece547fa1b56fe63e36932ed8caa8704e2b77b9
nsilverio <natalisilverio@gmail.com>
2020-03-28 06:56:20 +0000

    Update db.js
    Hiding db credentials

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://natali:20Corona@ns-mongo-8nvrc.mongodb.net/test?retryWrites=true&w=majority" 
+    url: "mongodb+srv://username:password@ns-mongo-8nvrc.mongodb.net/test?retryWrites=true&w=majority" 
   }
